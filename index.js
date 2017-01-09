fbf77fc458c6e6935a97b96bcd3339e4a56f1fd2
martinmicunda <martinmicunda@hotmail.com>
2017-01-09 10:50:08 +0100

    fix(SCHED-6): makes modal movaebla

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ export * from './lib/ng-decorators.js';
 export * from './lib/abstract-model.js';
 export * from './lib/abstract-resource.js';
 export * from './lib/abstract-resource-mock.js';
+export * from './lib/moveable-modal.js';
